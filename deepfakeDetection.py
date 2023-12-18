dfd1244729fb0a2b8d38b3dd676692bbdfbe3d9e
blanca <133573507+janeblancaocampo@users.noreply.github.com>
2023-12-18 19:07:35 +0800

    Update deepfakeDetection.py

--- deepfakeDetection.py
+++ deepfakeDetection.py
@@ -1,29 +1,9 @@
+User
 import streamlit as st
-import librosa.display
-import matplotlib.pyplot as plt
-from pydub import AudioSegment
-import io
 
 def process_audio_file(uploaded_file):
-    st.write("Generating waveform...")
-
-    # Convert the uploaded file to WAV format
-    audio_bytes = uploaded_file.getvalue()
-    audio = AudioSegment.from_file(io.BytesIO(audio_bytes))
-    wav_file = io.BytesIO()
-    audio.export(wav_file, format="wav")
-    wav_file.seek(0)
-
-    # Load the converted WAV file
-    audio_data, _ = librosa.load(wav_file, sr=None)
-
-    # Plot the waveform
-    plt.figure(figsize=(10, 4))
-    librosa.display.waveshow(audio_data, sr=44100)  # Adjust 'sr' according to your audio file's sample rate
-    plt.title('Audio Waveform')
-    plt.xlabel('Time')
-    plt.ylabel('Amplitude')
-    st.pyplot()
+    # Add your processing logic here
+    st.audio(uploaded_file, format='audio/wav')
 
 def main():
     st.title('Audio File Uploader')
