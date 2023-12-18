44d1939452031cd4b23381ce2a7d8e271fc63c7c
blanca <133573507+janeblancaocampo@users.noreply.github.com>
2023-12-18 18:53:19 +0800

    Update deepfakeDetection.py

--- deepfakeDetection.py
+++ deepfakeDetection.py
@@ -1,23 +1,8 @@
 import streamlit as st
-import soundfile as sf
-import matplotlib.pyplot as plt
-import numpy as np
 
 def process_audio_file(uploaded_file):
+    # Add your processing logic here
     st.audio(uploaded_file, format='audio/wav')
-
-    st.write("### Waveform of Uploaded Audio:")
-    with sf.SoundFile(uploaded_file) as audio_file:
-        audio_data = audio_file.read()
-        num_samples = len(audio_data)
-        duration = num_samples / audio_file.samplerate
-
-        time = np.linspace(0, duration, num_samples)
-        plt.figure(figsize=(10, 4))
-        plt.plot(time, audio_data)
-        plt.xlabel('Time (seconds)')
-        plt.ylabel('Amplitude')
-        st.pyplot()
 
 def main():
     st.title('Audio File Uploader')
