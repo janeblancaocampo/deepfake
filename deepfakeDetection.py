8325d8e98bbb8b2e4a27de838d1403bac2696c75
blanca <133573507+janeblancaocampo@users.noreply.github.com>
2023-12-18 17:54:52 +0800

    Update deepfakeDetection.py

--- deepfakeDetection.py
+++ deepfakeDetection.py
@@ -1,33 +1,22 @@
 import streamlit as st
-import sounddevice as sd
-import numpy as np
-import io
-from scipy.io.wavfile import write as write_wav
+from pydub import AudioSegment
 
-def record_audio(duration=6, fs=44100):
-    st.subheader("Recording...")
-    recording = sd.rec(int(duration * fs), samplerate=fs, channels=1, dtype=np.int16)
-    sd.wait()
-    st.subheader("Recording ended.")
-    return recording
+def is_within_6_seconds(audio_file):
+    audio = AudioSegment.from_file(audio_file)
+    duration = len(audio) / 1000  # Duration in seconds
+    return duration <= 6
 
 def main():
-    st.title('Audio File Uploader & Recorder')
+    st.title('Audio File Duration Checker')
 
-    option = st.radio("Choose an option:", ("Upload Audio File", "Record Audio"))
+    uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
 
-    if option == "Upload Audio File":
-        uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
-        if uploaded_file is not None:
-            st.audio(uploaded_file, format='audio/wav')
-
-    elif option == "Record Audio":
-        duration = st.slider("Recording duration (seconds):", min_value=1, max_value=10, value=6)
-        record_button = st.button("Start Recording")
-
-        if record_button:
-            recording = record_audio(duration)
-            st.audio(io.BytesIO(recording), format='audio/wav')
+    if uploaded_file is not None:
+        st.write("File uploaded successfully!")
+        if is_within_6_seconds(uploaded_file):
+            st.success("The audio file is within 6 seconds.")
+        else:
+            st.error("The audio file exceeds 6 seconds.")
 
 if __name__ == "__main__":
     main()
