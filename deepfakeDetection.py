382f8f0b140b1e738cb8379680195a7cb29f9e7d
blanca <133573507+janeblancaocampo@users.noreply.github.com>
2023-12-18 19:58:58 +0800

    Update deepfakeDetection.py

--- deepfakeDetection.py
+++ deepfakeDetection.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import librosa
+import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
 def process_audio_file(uploaded_file):
     # Add your processing logic here
     st.audio(uploaded_file, format='audio/wav')
 
     # Load audio using librosa
-    y, sr = librosa.load(uploaded_file)
+    y, sr = librosa.load(uploaded_file, sr=None)
 
     # Plot waveform
     plt.figure(figsize=(8, 4))
@@ -16,6 +18,17 @@ def process_audio_file(uploaded_file):
     plt.xlabel('Time')
     plt.ylabel('Amplitude')
     plt.plot(np.arange(len(y)) / sr, y)
+    st.pyplot()
+
+    # Create and display mel spectrogram
+    mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
+    mel_spectrogram_db = librosa.power_to_db(mel_spectrogram, ref=np.max)
+
+    plt.figure(figsize=(10, 4))
+    librosa.display.specshow(mel_spectrogram_db, sr=sr, x_axis='time', y_axis='mel')
+    plt.title('Mel Spectrogram')
+    plt.colorbar(format='%+2.0f dB')
+    plt.tight_layout()
     st.pyplot()
 
 def main():
