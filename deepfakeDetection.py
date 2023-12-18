213aa84cd3531b6516a3b676d04127092991354a
blanca <133573507+janeblancaocampo@users.noreply.github.com>
2023-12-18 17:57:18 +0800

    Update deepfakeDetection.py

--- deepfakeDetection.py
+++ deepfakeDetection.py
@@ -3,7 +3,7 @@ import streamlit as st
 def main():
     st.title('Audio File Uploader')
 
-    uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
+    uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "m4a"])
 
     if uploaded_file is not None:
         st.write("File uploaded successfully!")
