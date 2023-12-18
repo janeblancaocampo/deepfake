9f91c1eedc31251225f21caf9f57610869d0b898
blanca <133573507+janeblancaocampo@users.noreply.github.com>
2023-12-18 18:08:18 +0800

    Update deepfakeDetection.py

--- deepfakeDetection.py
+++ deepfakeDetection.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from io import BytesIO
 from pydub import AudioSegment
+from pydub.playback import play
 
 def main():
     st.title('Audio File Uploader')
@@ -11,11 +12,17 @@ def main():
         st.write("File uploaded successfully!")
 
         # Check if the file type is supported
-        if uploaded_file.type == "audio/mp3" or uploaded_file.type == "audio/wav" or uploaded_file.type == "audio/m4a":
-            st.audio(uploaded_file.read(), format=uploaded_file.type)
+        if uploaded_file.type == "audio/mp3" or uploaded_file.type == "audio/wav" or upload_file.type == "audio/m4a":
+            audio_bytes = uploaded_file.read()
+            st.audio(audio_bytes, format=uploaded_file.type)
+
+            # Play the audio
+            st.write("Playing the audio...")
+            audio_segment = AudioSegment.from_file(BytesIO(audio_bytes))
+            play(audio_segment)
 
         else:
-            st.write("Uploaded file type not supported. Please upload an MP3 or WAV file.")
+            st.write("Uploaded file type not supported. Please upload an MP3, M4A or WAV file.")
 
 if __name__ == "__main__":
     main()
