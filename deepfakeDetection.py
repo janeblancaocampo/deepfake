c90609abc5cdaf0e18f9ba7923a7c610f72c0ac1
blanca <133573507+janeblancaocampo@users.noreply.github.com>
2023-12-18 17:52:17 +0800

    Update deepfakeDetection.py

--- deepfakeDetection.py
+++ deepfakeDetection.py
@@ -1,22 +1,18 @@
 import streamlit as st
-from pydub import AudioSegment
 
-def is_within_6_seconds(audio_file):
-    audio = AudioSegment.from_file(audio_file)
-    duration = len(audio) / 1000  # Duration in seconds
-    return duration <= 6
+def process_audio_file(uploaded_file):
+    # Add your processing logic here
+    st.audio(uploaded_file, format='audio/wav')
 
 def main():
-    st.title('Audio File Duration Checker')
+    st.title('Audio File Uploader')
 
     uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav"])
 
     if uploaded_file is not None:
         st.write("File uploaded successfully!")
-        if is_within_6_seconds(uploaded_file):
-            st.success("The audio file is within 6 seconds.")
-        else:
-            st.error("The audio file exceeds 6 seconds.")
+        if st.button('Process Audio'):
+            process_audio_file(uploaded_file)
 
 if __name__ == "__main__":
     main()
